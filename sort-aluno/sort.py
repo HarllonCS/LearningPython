472c92a8496fa05b0d05170f26b302b297812667
HarllonCS <96022743+HarllonCS@users.noreply.github.com>
2022-08-01 11:29:46 -0300

    Ajuste - Sortear nome de aluno(a)

--- sort-aluno/sort.py
+++ sort-aluno/sort.py
@@ -1,4 +1,10 @@
-from random import randint
+from random import choice
 
-randint(1, 4)
+al1 = input("\nAluno(a) 1: ")
+al2 = input("Aluno(a) 2: ")
+al3 = input("Aluno(a) 3: ")
+al4 = input("Aluno(a) 4: ")
 
+lista = [al1, al2, al3, al4]
+
+print(f"Escolhido(a): {choice(lista)}")
