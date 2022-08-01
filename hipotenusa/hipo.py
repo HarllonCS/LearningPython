b8be072762e6f35e88710e1a4931c9d4d4f23925
HarllonCS <96022743+HarllonCS@users.noreply.github.com>
2022-08-01 10:08:07 -0300

    hipotenusa

---
+++ hipotenusa/hipo.py
@@ -0,0 +1,6 @@
+from math import hypot
+
+ca_oposto = int(input("\nDigite o cateto oposto: "))
+ca_adjacente = int(input("\nDigite o cateto adjacente: "))
+
+print(f"\nHipotenusa: {hypot(ca_oposto, ca_adjacente)}cm|")
