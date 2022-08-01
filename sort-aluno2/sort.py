c8354e822a11ddead63dbd844b86e8e28ee674ee
HarllonCS <96022743+HarllonCS@users.noreply.github.com>
2022-08-01 11:50:11 -0300

    Ajustes - Sortear nomes de pessoas em uma lista

--- sort-aluno2/sort.py
+++ sort-aluno2/sort.py
@@ -1,3 +1,12 @@
-from random import randint
+from random import shuffle, random
 
-randint(1, 4)
+al1 = input("\nAluno(a) 1: ")
+al2 = input("Aluno(a) 2: ")
+al3 = input("Aluno(a) 3: ")
+al4 = input("Aluno(a) 4: ")
+
+lista = [al1, al2, al3, al4]
+
+shuffle(lista)
+
+print(lista)
