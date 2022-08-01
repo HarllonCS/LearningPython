7b7553727169cb6528014642bafb89672bfd36c8
HarllonCS <96022743+HarllonCS@users.noreply.github.com>
2022-08-01 11:22:04 -0300

    Ajustes - seno-co-tan

--- co-e-tan/co-tan.py
+++
@@ -1,5 +0,0 @@
-from math import cos, tan
-
-angulo = int(input("Digite o ângulo: "))
-
-print(f"\nCosseno: {cos(angulo)}\nTangente: {tan(angulo)}")
---
+++ sen-co-tan/angulo.py
@@ -0,0 +1,5 @@
+from math import cos, tan, sin, radians
+
+angulo = radians(float(input("\nDigite o ângulo: ")))
+
+print(f"\nSeno: {sin(angulo):.2f}\nCosseno: {cos(angulo):.2f}\nTangente: {tan(angulo):.2f}")
