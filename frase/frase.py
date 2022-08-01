fcf322c77d908028662908f33ec9b1d29fcef51e
HarllonCS <96022743+HarllonCS@users.noreply.github.com>
2022-08-01 15:14:52 -0300

    Ajustes - 'frase'

--- frase/frase.py
+++ frase/frase.py
@@ -1,7 +1,7 @@
-frase = input("\nDigite uma frase qualquer: ").upper()
+frase = input("\nDigite uma frase qualquer: ").upper().strip()
 
 print(f"\nQuantidade de vezes que aparece a letra 'A': {frase.count('A')}")
 
-print(f"Posição em que a letra 'A' aparece pela primeria vez: [{frase.find('A')}]")
+print(f"Posição em que a letra 'A' aparece pela primeria vez: [{frase.find('A')+1}]")
 
-print(f"Posição em que a letra 'A' aparece pela última vez: [{frase.rfind('A')}]")
+print(f"Posição em que a letra 'A' aparece pela última vez: [{frase.rfind('A')+1}]")
