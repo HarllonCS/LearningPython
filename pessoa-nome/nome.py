e6fa9d218a9a8ec9bbbf1444dc007d98e07674c6
HarllonCS <96022743+HarllonCS@users.noreply.github.com>
2022-08-01 14:23:02 -0300

    Ajustes - pessoa-nome

--- pessoa-nome/nome.py
+++ pessoa-nome/nome.py
@@ -1,10 +1,10 @@
-nome = input("\nDigite um nome: ")
+nome = input("\nDigite um nome: ").strip()
 nome_split = nome.split()
 
 print(f"\nMaiúsculo: {nome.upper()}")
 
 print(f"Minúsculo: {nome.lower()}")
 
-print(f"Quantidade de caracteres: {len(nome)}")
+print(f"Quantidade de caracteres: {len(nome) - nome.count(' ')}")
 
 print(f"Quantidade de caracteres do primeiro nome: {len(nome_split[0])}")
