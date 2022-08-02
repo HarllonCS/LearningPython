2f5f7a795b7fb32a4a8931a56dd3485dbe795126
HarllonCS <96022743+HarllonCS@users.noreply.github.com>
2022-08-02 13:00:27 -0300

    Ajustes - 'ano-bissexto'

--- ano-bissexto/ano.py
+++ ano-bissexto/ano.py
@@ -1,6 +1,11 @@
+from datetime import date as d
+
 ano = int(input("\nDigite o ano: "))
 
-if ano%4 == 0 :
+if ano == 0 :
+    ano = d.today().year
+
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0 :
     print(f"\n{ano} é Bissexto!")
 else :
     print(f"\n{ano} não é Bissexto!")
---
+++ maior-menor/num.py
@@ -0,0 +1,4 @@
+n1 = int(input("\nDigite o primeiro número: "))
+n2 = int(input("\nDigite o segundo número: "))
+n3 = int(input("\nDigite o terceiro número: "))
+
