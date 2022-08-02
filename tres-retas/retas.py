8faec9f7522692d5ee286a136e5cabf415e04a9f
HarllonCS <96022743+HarllonCS@users.noreply.github.com>
2022-08-02 13:35:34 -0300

    'três retas'

--- tres-retas/retas.py
+++ tres-retas/retas.py
@@ -2,7 +2,7 @@ r1 = float(input("\nReta 1: "))
 r2 = float(input("\nReta 2: "))
 r3 = float(input("\nReta 3: "))
 
-if r1+r2 > r3 :
+if r2 + r3 > r1 and r1 + r3 > r2 and r1 + r2 > r3 :
     print(f"\nÉ um triângulo")
 else :
     print(f"\nNão é um triângulo")
