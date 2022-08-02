f61848d1a3b9c5b55ee1b6f322d4c180ab1e1c6a
HarllonCS <96022743+HarllonCS@users.noreply.github.com>
2022-08-02 12:34:33 -0300

    Ajustes - 'viagem'

--- viagem/viagem.py
+++ viagem/viagem.py
@@ -2,6 +2,7 @@ print(f"\n|{'-'*8}> Início <{'-'*8}|")
 
 viagem = float(input("\nDigite a viagem em km: "))
 
+"""
 if viagem <= 200 :
     preco = viagem * .50
     print(f"\nO preço da viagem custará: R${preco:.2f}")
@@ -9,5 +10,10 @@ else :
     preco = viagem * .45
     print(f"\nViagens mais longas como essa custarão menos!")
     print(f"\nO preço da viagem custará: R${preco:.2f}")
+"""
+
+preco = viagem * .50 if viagem <= 200 else viagem * .45
+
+print(f"\nO preço da viagem custará: R${preco:.2f}")
     
 print(f"\n|{'-'*8}> Fim <{'-'*8}|")
