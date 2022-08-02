9993982c397606ff31929f262b9c0268591aec47
HarllonCS <96022743+HarllonCS@users.noreply.github.com>
2022-08-02 10:31:34 -0300

    Número par ou ímpar

---
+++ par-impar/par-impar.py
@@ -0,0 +1,11 @@
+print(f"\n{'-'*8}| Inicio |{'-'*8}")
+
+num = int(input("\nDigite um numero inteiro: "))
+
+if num%2 == 0 :
+    print(f"\nO numero {num} eh par!")
+else :
+    print(f"\nO numero {num} eh impar!")
+    
+print(f"\n{'-'*8}| Inicio |{'-'*8}")
+    
