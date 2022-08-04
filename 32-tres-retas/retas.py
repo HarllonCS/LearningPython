f59ea35af3daefbcb8bc176477437746a6602398
HarllonCS <96022743+HarllonCS@users.noreply.github.com>
2022-08-03 21:39:46 -0300

    'tres-retas' atualizado

--- 32-tres-retas/retas.py
+++ 32-tres-retas/retas.py
@@ -2,7 +2,17 @@ r1 = float(input("\nReta 1: "))
 r2 = float(input("\nReta 2: "))
 r3 = float(input("\nReta 3: "))
 
-if r2 + r3 > r1 and r1 + r3 > r2 and r1 + r2 > r3 :
-    print(f"\nÉ um triângulo")
+if (r2 + r3 > r1) and (r1 + r3 > r2) and (r1 + r2 > r3) :
+    if r1 == r2 == r3 :
+        txt = "\n\nTriângulo Equilátero."
+        
+    elif r1 != r2 != r3 :
+        txt = "\n\nTriângulo Escaleno."
+    
+    else :
+        txt = "\n\nTriângulo Isósceles"
+    
 else :
-    print(f"\nNão é um triângulo")
+    txt = "\nNão é um triângulo!"
+    
+print(txt)
