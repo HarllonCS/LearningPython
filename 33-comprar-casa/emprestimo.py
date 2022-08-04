f56ddd0d5e711bc6ad32cac951fdc7e320aba801
HarllonCS <96022743+HarllonCS@users.noreply.github.com>
2022-08-04 10:28:13 -0300

    emprestimo

--- 33-comprar-casa/emprestimo.py
+++ 33-comprar-casa/emprestimo.py
@@ -3,6 +3,8 @@ salario = float(input("Informe seu salário: "))
 anos = int(input("Digite em quantos anos você vai pagar: "))
 
 valor = (casa_valor / anos) / 12
+minimo = salario * 30 / 100
 
 print(f"\nValor da Prestação Mensal: R${valor:.2f}")
 
+print("\nEmpréstimo pode ser consedido." if valor <= minimo else "\nEmpréstimo negado.")
