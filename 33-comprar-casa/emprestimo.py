4928f75c4f78bb31602a14967f146906fe3cc676
HarllonCS <96022743+HarllonCS@users.noreply.github.com>
2022-08-03 22:10:11 -0300

    'comprar-casa' e 'imc'

---
+++ 33-comprar-casa/emprestimo.py
@@ -0,0 +1,8 @@
+casa_valor = float(input("\nInforme o valor da casa: "))
+salario = float(input("Informe seu salário: "))
+anos = int(input("Digite em quantos anos você vai pagar: "))
+
+valor = (casa_valor / anos) / 12
+
+print(f"\nValor da Prestação Mensal: R${valor:.2f}")
+
---
+++ 39-imc/imc.py
@@ -0,0 +1,23 @@
+peso = float(input("\nDigite o peso: "))
+altura = float(input("Digite a altura: "))
+
+imc = peso / altura**2
+
+print(f"\nSeu índice de Massa Corporal(IMC) é de {imc:.1f}kg/m2")
+
+if imc < 18.5 :
+    txt = "\nVocê tem Magreza."
+
+elif imc >= 18.5 and imc < 25 :
+    txt = "\nSeu peso está Normal."
+    
+elif imc >= 25 and imc < 30 :
+    txt = "\nVocê está no Sobrepeso."
+    
+elif imc >= 30 and imc <= 40 :
+    txt = "\nVocê está com Obesidade."
+
+else :
+    txt = "\nVocê está com Obesidade Morbida."
+    
+print(txt)
