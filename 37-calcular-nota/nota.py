2ba11085c9bb9dff673ca0a95f98f07974056ebb
HarllonCS <96022743+HarllonCS@users.noreply.github.com>
2022-08-07 22:01:21 -0300

    'calcular-nota'

--- 37-calcular-nota/nota.py
+++ 37-calcular-nota/nota.py
@@ -5,13 +5,13 @@ n2 = float(input("Digie a 2ª nota: "))
 # Calcular média.
 media = (n1 + n2) / 2
 
-print(f"\nSua média é  {media:.1f}")
+print(f"\nSua média é {media:.1f}")
 
 # Condição para verficar se o(a) aluno(a) foi aprovado(a) ou não.
 if media >= 7.0 :
     txt = "\nVocê foi aprovado(a)."
     
-elif media == 5.0 and media <= 6.9 :
+elif media >= 5.0 and media <= 6.9 :
     txt = "\nVocê está em recuperação."
     
 else :
