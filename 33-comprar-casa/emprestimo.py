20b3ab2b3ee669f083c4f80df80f32d8c99ae3cb
HarllonCS <96022743+HarllonCS@users.noreply.github.com>
2022-08-07 21:11:14 -0300

    'comprar-casa' e 'jokenpo'

--- 33-comprar-casa/emprestimo.py
+++ 33-comprar-casa/emprestimo.py
@@ -2,9 +2,9 @@ casa_valor = float(input("\nInforme o valor da casa: "))
 salario = float(input("Informe seu salário: "))
 anos = int(input("Digite em quantos anos você vai pagar: "))
 
-valor = (casa_valor / anos) / 12
+valor = casa_valor / (anos * 12)
 minimo = salario * 30 / 100
 
 print(f"\nValor da Prestação Mensal: R${valor:.2f}")
 
-print("\nEmpréstimo pode ser consedido." if valor <= minimo else "\nEmpréstimo negado.")
+print("\nEmpréstimo pode ser concedido." if valor <= minimo else "\nEmpréstimo negado.")
--- 41-jokenpo/jogo.py
+++ 41-jokenpo/jogo.py
@@ -4,28 +4,47 @@ print("\nPredra - Papel - Tesoura")
 
 pc = rd(1, 3)
 
-print("\nEscolha:\n\n1 - Pedra\n2 - Papel\n3 - Tesoura")
-
-usuario = int(input("\nUsuário: "))
-
 if pc == 1 :
     esc_pc = "Pedra"
 
 elif pc == 2 :
     esc_pc = "Papel"
-    
+
 else :
     esc_pc = "Tesoura"
-    
-    
+
+
+print("\nEscolha:\n\n1 - Pedra\n2 - Papel\n3 - Tesoura")
+
+usuario = int(input("\nEscolha: "))
+
 if usuario == 1 :
     esc_usu = "Pedra"
-    
+
 elif usuario == 2 :
     esc_usu = "Papel"
-    
+
 elif usuario == 3 :
     esc_usu = "Tesoura"
-    
+
 else :
     print("\nEssa opção não existe!")
+    
+    
+#print("\nResultado".upper())
+
+usu_perdeu = (pc == 1 and usuario == 3) or (pc == 2 and usuario == 1) or (pc == 3 and usuario == 2)
+
+usu_venceu = (usuario == 1 and pc == 3) or (usuario == 2 and pc == 1) or (usuario == 3 and pc == 2)
+
+print(f"\nPC: {esc_pc}\nUsuário: {esc_usu}")
+
+if usu_venceu :
+    print("\nVocê é foda!".upper())
+
+elif usu_perdeu :
+    print("\nPerdeu otário!! Mais sorte na próxima vez...")
+
+else :
+    print("\nEmpate!!".upper())
+    
