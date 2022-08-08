cd69113c065ad6755374e5849ad1d2b6478f4f6e
HarllonCS <96022743+HarllonCS@users.noreply.github.com>
2022-08-08 11:32:32 -0300

    'pagamento'

--- 40-pagamento/pagar.py
+++ 40-pagamento/pagar.py
@@ -1,4 +1,4 @@
-preco = 400.00
+preco = float(input("\nPreço total dos produtos: "))
 
 print("\nInisira a forma de pagamento: ")
 print("\n1 - Á vista no dinheiro/cheque\n2 - Á vista no cartão\n3 - Até 2x no cartão\n4 - 3x ou mais no cartão\n")
@@ -6,18 +6,22 @@ print("\n1 - Á vista no dinheiro/cheque\n2 - Á vista no cartão\n3 - Até 2x n
 op = int(input("Digite aqui: "))
 
 if op == 1 :
-    preco -= ((preco * 10) / 100)
-    txt = f"\nVocê recebeu 10% de desconto.\nO preço ficou por R${preco:.2f}!"
+    preco -= (preco * (10 / 100))
+    txt = f"\nVocê recebeu 10% de desconto.\nPreço Total: R${preco:.2f}!"
     
 elif op == 2 :
-    preco -= ((preco * 5) / 100)
-    txt = f"\nVocê recebeu 5% de desconto.\nO preço ficou por R${preco:.2f}!"
+    preco -= (preco * (5 / 100))
+    txt = f"\nVocê recebeu 5% de desconto.\nPreço Total: R${preco:.2f}!"
     
 elif op == 3 :
-    txt = f"\nVocê não recebeu desconto e nem juros.\nPreço: R${preco:.2f}!"
+    preco /= 2
+    txt = f"\nVocê terá que pagar parcelado 2x.\nPreço Total: R${preco:.2f}!"
     
 elif op == 4 :
-    txt = f"\nVocê terá que pagar 20x de juros."
+    preco += (preco * (20 / 100))
+    parcelas = int(input("\nParcelas à pagar: "))
+    preco /= parcelas
+    txt = f"\nVocê terá que pagar parcelado em {parcelas}x com 20% de juros.\nPreco Total: R${preco:.2f}!"
     
 else :
     txt = f"\nEssa opção não existe!"
