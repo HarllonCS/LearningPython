670973ce3860646724bb2adc02e2340e84d2fd9a
HarllonCS <harllon.santana@gmail.com>
2022-09-17 14:43:19 -0300

    número primo

---
+++ 48-num-primo/num.py
@@ -0,0 +1,15 @@
+num = int(input("\nDigite um número: "))
+
+mult = 0
+
+for x in range(1, num+1) :
+    
+    if num % x == 0 :
+        print(f"\n{num} é mútiplo de: {x}")
+        mult += 1
+
+if mult == 2 :
+    print(f"\n{num} é primo!\nPois, ele divisível apenas por {mult} números: 1 e {num}")
+
+else :
+    print(f"\n{num} não é número primo, pois ele mútiplo de {mult} números.")
